8af602397d67a3049bb5f77395935a717e0b8bce
fenghaowei <fhw5629@gmail.com>
2019-09-28 00:08:43 +0800

    commit

--- common/logger.js
+++ common/logger.js
@@ -4,12 +4,19 @@ const pathLib = require('path')
 const env = process.env.NODE_ENV || 'development'
 
 const log4js = require('log4js')
-// log4js.configure({
-//   appenders: [
-//     {type: 'console'},
-//     {type: 'file', filename: pathLib.join(config.log_dir, 'cheese.log')}
-//   ]
-// })
+log4js.configure({
+    appenders: {
+        'out': {
+            type: 'stdout',
+            layout: {
+                type: 'pattern', pattern: '%[[%d{yyyy-MM-dd hh:mm:ss}][%p][%c] %f{2}-%l%] %m'
+            }
+        }
+    },
+    categories: {
+        default: {appenders: ['out'], level: 'info', enableCallStack: true}
+    }
+})
 
 const logger = log4js.getLogger()
 logger.level = (config.debug && env !== 'test' ? 'DEBUG' : 'ERROR')
